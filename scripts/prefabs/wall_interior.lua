0a7921c852ffc3561b54ab8324daee575e2e4ddf
KaiserKatze <donizyo@gmail.com>
2019-01-28 12:54:18 +0800

    Format

--- scripts/prefabs/wall_interior.lua
+++ scripts/prefabs/wall_interior.lua
@@ -1,48 +1,48 @@
 local assets=
 {
-	Asset("ANIM", "anim/interior_wall_noseam.zip"),
+  Asset("ANIM", "anim/interior_wall_noseam.zip"),
 }
 
 local prefabs =
 {
 
-}    
+}
 
 local function onsave(inst, data)
-    if inst.rotation then
-        data.rotation = inst.rotation 
-    end
+  if inst.rotation then
+    data.rotation = inst.rotation
+  end
 end
 
 local function onload(inst, data)
-    if data then
-        if data.rotation then
-            inst.Transform:SetRotation(data.rotation)
-            inst.rotation = data.rotation
-        end
+  if data then
+    if data.rotation then
+      inst.Transform:SetRotation(data.rotation)
+      inst.rotation = data.rotation
     end
+  end
 end
 
 local function fn(Sim)
-	local inst = CreateEntity()
-	inst.entity:AddTransform()
-	
-	inst.entity:AddAnimState()
-    inst.AnimState:SetBank("interior_wall")
-    inst.AnimState:SetBuild("interior_wall_noseam")
-    inst.AnimState:PlayAnimation("idle",true)
-    inst.AnimState:SetOrientation(ANIM_ORIENTATION.RotatingBillboard)
+  local inst = CreateEntity()
+  inst.entity:AddTransform()
 
-    inst.AnimState:SetLayer( LAYER_BACKGROUND )
-    inst.AnimState:SetSortOrder( 3 )      
-    inst.AnimState:SetScale(4,4,4 )      
-    --inst.AnimState:SetRayTestOnBB(true);
+  inst.entity:AddAnimState()
+  inst.AnimState:SetBank("interior_wall")
+  inst.AnimState:SetBuild("interior_wall_noseam")
+  inst.AnimState:PlayAnimation("idle",true)
+  inst.AnimState:SetOrientation(ANIM_ORIENTATION.RotatingBillboard)
 
-    --------SaveLoad
-    inst.OnSave = onsave 
-    inst.OnLoad = onload 
+  inst.AnimState:SetLayer(LAYER_BACKGROUND)
+  inst.AnimState:SetSortOrder(3)
+  inst.AnimState:SetScale(4,4,4)
+  --inst.AnimState:SetRayTestOnBB(true);
 
-    return inst
+  --------SaveLoad
+  inst.OnSave = onsave
+  inst.OnLoad = onload
+
+  return inst
 end
 
-return Prefab( "interior/objects/wall_interior", fn, assets, prefabs)
+return Prefab("interior/objects/wall_interior", fn, assets, prefabs)
--- scripts/prefabs/wall_test.lua
+++ scripts/prefabs/wall_test.lua
@@ -1,23 +1,23 @@
 local assets=
 {
-    Asset("ANIM", "anim/pig_room_general.zip"),
-    Asset("ANIM", "anim/pig_room_wood.zip"),
+  Asset("ANIM", "anim/pig_room_general.zip"),
+  Asset("ANIM", "anim/pig_room_wood.zip"),
 }
 
 local function fn(Sim)
-	local inst = CreateEntity()
-	local trans = inst.entity:AddTransform()
-	local anim = inst.entity:AddAnimState()
-    inst.entity:AddSoundEmitter()
+  local inst = CreateEntity()
+  local trans = inst.entity:AddTransform()
+  local anim = inst.entity:AddAnimState()
+  inst.entity:AddSoundEmitter()
 
-    anim:SetBank("pig_room")
-    anim:SetBuild("pig_room_general")
-    anim:PlayAnimation("idle", true)
+  anim:SetBank("pig_room")
+  anim:SetBuild("pig_room_general")
+  anim:PlayAnimation("idle", true)
 
-    anim:OverrideSymbol("wall_side1", "pig_room_wood", "wall_side1")
-    anim:Hide("wall_side2")
+  anim:OverrideSymbol("wall_side1", "pig_room_wood", "wall_side1")
+  anim:Hide("wall_side2")
 
-    return inst
+  return inst
 end
 
-return Prefab( "common/wall_test", fn, assets) 
+return Prefab("common/wall_test", fn, assets)
