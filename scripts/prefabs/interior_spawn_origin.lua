acc6ff68e4f2628fc8d3d01c7e069447982228dc
KaiserKatze <donizyo@gmail.com>
2019-01-31 21:57:38 +0800

    Format

--- scripts/prefabs/interior_spawn_origin.lua
+++ scripts/prefabs/interior_spawn_origin.lua
@@ -1,28 +1,28 @@
-local assets=
+local assets =
 {
 }
 
 local prefabs =
 {
-}    
-
+}
 
 local function fn(Sim)
 	local inst = CreateEntity()
 	inst.entity:AddTransform()
 	inst:AddTag("NOBLOCK")
-    inst:AddTag("interior_spawn_origin")
+  inst:AddTag("interior_spawn_origin")
 
-    return inst
+  return inst
 end
+
 local function storagefn(Sim)
 	local inst = CreateEntity()
 	inst.entity:AddTransform()
 	inst:AddTag("NOBLOCK")
-    inst:AddTag("interior_spawn_storage")
+  inst:AddTag("interior_spawn_storage")
 
-    return inst
+  return inst
 end
 
-return Prefab( "forest/objects/interior_spawn_origin", fn, assets, prefabs),
-	   Prefab( "forest/objects/interior_spawn_storage", storagefn, assets, prefabs)
+return Prefab("interior_spawn_origin", fn, assets, prefabs),
+  Prefab("interior_spawn_storage", storagefn, assets, prefabs)
