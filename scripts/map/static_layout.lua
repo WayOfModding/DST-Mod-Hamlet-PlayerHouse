a37f5f2cf1844cb964422678366d65fcd1ad0f8e
KaiserKatze <donizyo@gmail.com>
2019-01-31 23:12:41 +0800

    Copy from Hamlet

--- scripts/map/static_layout.lua
+++ scripts/map/static_layout.lua
@@ -1,7 +1,5 @@
 require "util"
 
-local PrefabSwaps = require("prefabswaps")
-
 local ParseNestedKey -- must define it first so we can recurse
 ParseNestedKey = function(obj, key, value)
 	if #key == 1 then
@@ -39,72 +37,90 @@ local function ConvertStaticLayoutToLayout(layoutsrc, additionalProps)
 	layout.ground_types = {
 							--Translates tile type index from constants.lua into tiled tileset. 
 							--Order they appear here is the order they will be used in tiled.
-							GROUND.IMPASSABLE,		GROUND.ROAD,				GROUND.ROCKY,		GROUND.DIRT,			GROUND.SAVANNA,		GROUND.GRASS,				GROUND.FOREST,			        GROUND.MARSH, 
-							GROUND.WOODFLOOR,		GROUND.CARPET,				GROUND.CHECKER,		GROUND.CAVE,			GROUND.FUNGUS,		GROUND.SINKHOLE,			GROUND.QUAGMIRE_GATEWAY,		GROUND.QUAGMIRE_SOIL, 
-							GROUND.IMPASSABLE,		GROUND.IMPASSABLE,			GROUND.IMPASSABLE,  GROUND.IMPASSABLE, 	    GROUND.UNDERROCK,	GROUND.MUD,					GROUND.QUAGMIRE_PEATFOREST,		GROUND.IMPASSABLE,
-							GROUND.BRICK,			GROUND.BRICK_GLOW,			GROUND.TILES,		GROUND.TILES_GLOW, 		GROUND.TRIM,		GROUND.TRIM_GLOW,			GROUND.QUAGMIRE_PARKSTONE,	    GROUND.QUAGMIRE_PARKFIELD,
-							GROUND.IMPASSABLE,	    GROUND.IMPASSABLE,	        GROUND.FUNGUSRED,	GROUND.FUNGUSGREEN,		GROUND.FAKE_GROUND,	GROUND.LAVAARENA_FLOOR,		GROUND.LAVAARENA_TRIM,          GROUND.QUAGMIRE_CITYSTONE,
-						}
+							GROUND.IMPASSABLE, GROUND.ROAD, GROUND.ROCKY, GROUND.DIRT, 
+							GROUND.SAVANNA, GROUND.GRASS, GROUND.FOREST, GROUND.MARSH, 
+
+							GROUND.WOODFLOOR, 		GROUND.CARPET, 				GROUND.CHECKER,				GROUND.CAVE,
+							GROUND.FUNGUS, 			GROUND.SINKHOLE, 			GROUND.WALL_ROCKY, 			GROUND.WALL_DIRT,
+							
+							GROUND.WALL_MARSH, 		GROUND.WALL_CAVE, 			GROUND.WALL_FUNGUS, 		GROUND.WALL_SINKHOLE, 
+							GROUND.UNDERROCK, GROUND.MUD, GROUND.WALL_MUD, GROUND.WALL_WOOD,
+
+							GROUND.BRICK, 			GROUND.BRICK_GLOW, 			GROUND.TILES, 				GROUND.TILES_GLOW,  
+							GROUND.TRIM, GROUND.TRIM_GLOW, GROUND.WALL_HUNESTONE, GROUND.WALL_HUNESTONE_GLOW,
+
+							GROUND.WALL_STONEEYE, GROUND.WALL_STONEEYE_GLOW, GROUND.FUNGUSRED, GROUND.FUNGUSGREEN,
+							GROUND.BEACH, 			GROUND.JUNGLE, 				GROUND.SWAMP, 				GROUND.OCEAN_SHALLOW, 	
+
+							GROUND.OCEAN_MEDIUM, 	GROUND.OCEAN_DEEP,			GROUND.OCEAN_CORAL, 		GROUND.MANGROVE,
+							GROUND.MAGMAFIELD, 		GROUND.TIDALMARSH, 			GROUND.MEADOW,				GROUND.VOLCANO, 		
+
+							GROUND.VOLCANO_LAVA, 	GROUND.ASH, 				GROUND.VOLCANO_ROCK, 		GROUND.OCEAN_SHIPGRAVEYARD,
+							GROUND.COBBLEROAD,		GROUND.FOUNDATION,			GROUND.DEEPRAINFOREST,		GROUND.LAWN,
+
+							GROUND.PIGRUINS, 		GROUND.LILYPOND, 			GROUND.GASJUNGLE,			GROUND.SUBURB,
+							GROUND.RAINFOREST,		GROUND.PIGRUINS_NOCANOPY,	GROUND.PLAINS,				GROUND.PAINTED,
+
+							GROUND.BATTLEGROUND, 	GROUND.INTERIOR, 			GROUND.FIELDS					}
 	layout.ground = {}
-	
+
 	-- so we can support both 16 wide grids and 64 wide grids from tiled
 	local tilefactor = math.ceil(64/staticlayout.tilewidth)
-
+	
 	-- See \tools\tiled\dont_starve\objecttypes.xml for objects
 	layout.layout = {}
-
+	
 	for layer_idx, layer in ipairs(staticlayout.layers) do
-		if layer.type == "tilelayer" and layer.name == "BG_TILES" then
+		if layer.type == "tilelayer" and layer.name == "BG_TILES" then 
 			local val_per_row = layer.width * (tilefactor-1)
 			local i = val_per_row
 
-			while i < #layer.data do
-				local data = {}
+			while i < #layer.data do		
+				local data = {}	
 				local j = 1
-				while j <= layer.width and i+j <= #layer.data do
+				while j < layer.width and i+j < #layer.data do
 					table.insert(data, layer.data[i+j])
 					j = j + tilefactor
 				end
-				table.insert(layout.ground, data)
+				table.insert(layout.ground, data)	
 				i = i + val_per_row + layer.width
 			end
-		elseif layer.type == "objectgroup" and string.find(layer.name, "FG_OBJECTS") then
+		elseif layer.type == "objectgroup" and layer.name == "FG_OBJECTS" then 
 			for obj_idx, obj in ipairs(layer.objects) do
-                if not PrefabSwaps.IsPrefabInactive(obj.type) then
-                    local prefab = PrefabSwaps.ResolvePrefabProxy(obj.type)
+				if layout.layout[obj.type] == nil then
+					layout.layout[obj.type] = {}
+				end
+				
+				-- TODO: Check the object properties for other options to substitute here
+				local x = obj.x+obj.width/2
+				x = x/64.0-(staticlayout.width/tilefactor)/2
+				local y = obj.y+obj.height/2
+				y = y/64.0-(staticlayout.height/tilefactor)/2
 
-    				if layout.layout[prefab] == nil then
-    					layout.layout[prefab] = {}
-    				end
+				local width = obj.width/64.0
+				local height = obj.height/64.0
 
-    				-- TODO: Check the object properties for other options to substitute here
-    				local x = obj.x+obj.width/2
-    				x = x/64.0-(staticlayout.width/tilefactor)/2
-    				local y = obj.y+obj.height/2
-    				y = y/64.0-(staticlayout.height/tilefactor)/2
+				local properties = {}
+				if obj.properties then
+					for k,v in pairs(obj.properties) do
+						local keys = k:split(".")
+						local number_v = tonumber(v)
+						if v == "true" or v == "false" then
+							ParseNestedKey(properties,keys, v == "true")
+						else
+							ParseNestedKey(properties,keys,number_v or v)
+						end
+					end
+					
+					--print("Static Layout Properties for ", layoutsrc)
+					--dumptable(properties,1,10)
 
-    				local width = obj.width/64.0
-    				local height = obj.height/64.0
+				end
 
-    				local properties = {}
-    				if obj.properties then
-    					for k,v in pairs(obj.properties) do
-    						local keys = k:split(".")
-    						local number_v = tonumber(v)
-    						if v == "true" or v == "false" then
-    							ParseNestedKey(properties,keys, v == "true")
-    						else
-    							ParseNestedKey(properties,keys,number_v or v)
-    						end
-    					end
-
-    					--print("Static Layout Properties for ", layoutsrc)
-    					--dumptable(properties,1,10)
-
-    				end
-
-    				table.insert(layout.layout[prefab], {x=x, y=y, properties=properties, width=width, height=height})				
-                end
+				if not properties.chance or math.random() < properties.chance then
+					table.insert(layout.layout[obj.type], {x=x, y=y, properties=properties, width=width, height=height})				
+				end
+			
 			end
 
 			if layout.initfn then
