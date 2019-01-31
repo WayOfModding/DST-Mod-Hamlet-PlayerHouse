d2b60da497f283526ef068f01f21c3d34093fa29
KaiserKatze <donizyo@gmail.com>
2019-01-31 16:04:50 +0800

    Copy from Hamlet

---
+++ scripts/modrecipes.lua
@@ -0,0 +1,1 @@
+Recipe("playerhouse_city", {Ingredient("boards", 4), Ingredient("cutstone", 3), Ingredient("oinc", 30)}, RECIPETABS.CITY, TECH.CITY, RECIPE_GAME_TYPE.PORKLAND, "playerhouse_city_placer")
