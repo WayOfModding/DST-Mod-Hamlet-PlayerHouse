e66150467c23c70fd00a8513be3aee023d359a1a
KaiserKatze <donizyo@gmail.com>
2019-02-23 09:57:16 +0800

    Update

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,17 @@
 --The name of the mod displayed in the 'mods' screen.
-name = "[Hamlet] Slanty Shanty"
+name = "[Hamlet] Porkland"
 
---A description of the mod.
-description = "Slanty Shanty from Hamlet DLC of Don't Starve."
+--A version number so you can ask people if they are running an old version of your mod.
+version = "1.0.0"
 
 --Who wrote this awesome mod?
 author = "KaiserKatze"
 
---A version number so you can ask people if they are running an old version of your mod.
-version = "1.0.0"
+--A description of the mod.
+description = "Version "..version.." By "..author..[[
+
+Port Hamlet DLC of Don't Starve.
+]]
 
 --This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
 api_version = 10
